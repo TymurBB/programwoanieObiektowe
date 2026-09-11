606414f81f309dc2107fb75550984aafecf20a96
Tymur Bosenko <bosenkotymur4@gmail.com>
2026-09-11 09:22:43 +0200

    hello world

--- cwiczenie01/src/Main.java
+++ cwiczenie01/src/Main.java
@@ -1,13 +1,16 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+    System.out.println("hello world");
+    System.out.println("skrót");//przez skrót
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
-    }
+    System.out.print("coś bez entera");
+    System.out.println("nowa linia");
+
+    System.out.println("W taki sposob robie znaki specjalne");
+    System.out.println("\ttabulacja");
+    System.out.println("\nenter");
+    System.out.println("To jest jak na maszynie\rdo pisania");
+    System.out.println("\" cudzysłów\"");
+    System.out.println("\'apostrow\'");
+    System.out.println("backslasch: \\");
+
 }
